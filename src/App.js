6ba25c9463c2e22fbdde7cfc4c45907f75d015df
김지이 <k63399309@gmail.com>
2022-09-21 11:55:27 +0900

    최초업로드

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import Title from "./components/Title";
+import Nav from "./components/Nav";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
+
+import './App.scss';
+
+const App = ()=>{
+  return(
+    <>
+      <Header>
+        <Title />
+        <Nav />
+      </Header>
+
+      <Section />
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
